b91a323ba1edfdc4333154c9e404aa10eba87904
jermaine-f1 <111958346+jermaine-f1@users.noreply.github.com>
2025-01-17 05:46:34 +0000

    add grok-2-latest

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ if "selected_model" not in st.session_state:
 
 # Define model details
 models = {
+    "grok-2-latest": {"name": "grok-2-latest"},
     "gemma2-9b-it": {"name": "Gemma2-9b-it", "tokens": 8192, "developer": "Google"},
     "llama-3.3-70b-versatile": {"name": "LLaMA3.3-70b-versatile", "tokens": 128000, "developer": "Meta"},
     "llama-3.1-8b-instant" : {"name": "LLaMA3.1-8b-instant", "tokens": 128000, "developer": "Meta"},
